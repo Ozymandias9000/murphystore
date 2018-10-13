f44fc59c458b89ce2699f9a2dd45de88fd1604bd
Ozymandias9000 <[nick_murphy@sbcglobal.net]>
2018-10-12 20:27:58 -0700

    Item schema and first mutation

--- sick-fits/backend/src/createServer.js
+++ sick-fits/backend/src/createServer.js
@@ -7,6 +7,16 @@ const db = require("./db");
 
 function createServer() {
   return new GraphQLServer({
-    typeDefs: "src/schema.graphql"
+    typeDefs: "src/schema.graphql",
+    resolvers: {
+      Mutation,
+      Query
+    },
+    resolverValidationOptions: {
+      requireResolversForResolveType: false
+    },
+    context: req => ({ ...req, db })
   });
 }
+
+module.exports = createServer;
--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,17 @@
-const Mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // Check login
+
+    const item = await ctx.db.mutation.this.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
+    return item;
+  }
+};
 
 module.exports = Mutations;
