4af33a945419e9860dd81a53f7d922c048ff21b6
Ozymandias9000 <[nick_murphy@sbcglobal.net]>
2018-10-14 13:08:02 -0700

    Fixed item query & made item & itemlist components

--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
+import Items from "../components/Items";
 
 export default class Home extends Component {
   render() {
-    return <div>hey!</div>;
+    return <Items />;
   }
 }
