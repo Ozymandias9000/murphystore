470c8bdedb9bd3c016f2ed4794fe17e6aedf8e57
Ozymandias9000 <[nick_murphy@sbcglobal.net]>
2018-10-20 11:41:52 -0700

    Add to cart working back and front end

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -201,28 +201,36 @@ const Mutations = {
     if (!userId) throw new Error("You must be signed in!");
     // 2. Query current cart
     const [existingCartItem] = await ctx.db.query.cartItems({
-      user: { id: userId },
-      item: { id: args.id }
+      where: {
+        user: { id: userId },
+        item: { id: args.id }
+      }
     });
     // 3. Check if item in cart - if so increment
     if (existingCartItem) {
       console.log("Already in");
-      return ctx.db.mutation.updateCartItem({
-        where: { id: existingCartItem.id },
-        data: { quantity: existingCartItem.quantity + 1 }
-      });
+      return ctx.db.mutation.updateCartItem(
+        {
+          where: { id: existingCartItem.id },
+          data: { quantity: existingCartItem.quantity + 1 }
+        },
+        info
+      );
     }
     // 4. If not create fresh CartItem for user
-    return ctx.db.mutation.createCartItem({
-      data: {
-        user: {
-          connect: { id: userId }
-        },
-        item: {
-          connect: { id: args.id }
+    return ctx.db.mutation.createCartItem(
+      {
+        data: {
+          user: {
+            connect: { id: userId }
+          },
+          item: {
+            connect: { id: args.id }
+          }
         }
-      }
-    });
+      },
+      info
+    );
   }
 };
 
