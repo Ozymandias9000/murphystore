e9867c63e17778a69ad0770ab2606bc0702171b5
Ozymandias9000 <[nick_murphy@sbcglobal.net]>
2018-10-20 10:28:43 -0700

    Working on adding items to cart

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -194,6 +194,35 @@ const Mutations = {
       },
       info
     );
+  },
+  async addToCart(parent, args, ctx, info) {
+    // 1. Make sure signed in
+    const { userId } = ctx.request;
+    if (!userId) throw new Error("You must be signed in!");
+    // 2. Query current cart
+    const [existingCartItem] = await ctx.db.query.cartItems({
+      user: { id: userId },
+      item: { id: args.id }
+    });
+    // 3. Check if item in cart - if so increment
+    if (existingCartItem) {
+      console.log("Already in");
+      return ctx.db.mutation.updateCartItem({
+        where: { id: existingCartItem.id },
+        data: { quantity: existingCartItem.quantity + 1 }
+      });
+    }
+    // 4. If not create fresh CartItem for user
+    return ctx.db.mutation.createCartItem({
+      data: {
+        user: {
+          connect: { id: userId }
+        },
+        item: {
+          connect: { id: args.id }
+        }
+      }
+    });
   }
 };
 
